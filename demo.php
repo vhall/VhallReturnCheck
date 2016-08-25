5304f66650f873d3f98d1e81dee04438b95377d2
GaoYan <yan.gao@vhall.com>
2016-08-25 17:18:39 +0800

    fix file_put_content to show

--- demo.php
+++ demo.php
@@ -10,6 +10,7 @@ $return = $checkSign->check();
 if ($return['code'] == 200) {
     var_dump($return);
 
+    //file_put_contents('json.txt', json_encode($return));
     // list of return data
     // user_id fee note type
 } else {
